e98691f58e8bf4d0d74c06853a4f203c0975dc4d
DarkSharpness <2040703891@qq.com>
2024-04-16 00:40:04 +0800

    Upd(page): Add back debug message.

--- os/src/alloc/page.rs
+++ os/src/alloc/page.rs
@@ -266,6 +266,8 @@ unsafe fn set_normal_identity(mut page : PageAddress, i : usize, j : usize, k : 
 unsafe fn allocate_zero() -> PageAddress {
     let addr = BuddyAllocator::allocate_page();
 
+    warning!("Zero-filled page allocated at {:p}", addr);
+
     /* Reset the page to zero. */
     let temp = addr as *mut u64;
     for i in 0..512 { *temp.wrapping_add(i) = 0; }
@@ -275,5 +277,6 @@ unsafe fn allocate_zero() -> PageAddress {
 
 unsafe fn allocate_page() -> PageAddress {
     let addr = BuddyAllocator::allocate_page();
+    warning!("Uninitialized page allocated at {:p}", addr);
     return PageAddress::new_ptr(addr);
 }
