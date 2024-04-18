f1a81bddd70d919c9aa4c813ffd9c33b0fec6ff8
DarkSharpness <2040703891@qq.com>
2024-04-18 23:29:26 +0800

    refactor(sys): change enum into u64 for flexibility in code

--- sys/src/syscall.rs
+++ sys/src/syscall.rs
@@ -1,25 +1,24 @@
-#[allow(dead_code)]
-enum SyscallIndex {
-    SYSFORK     = 1,
-    SYSEXIT     = 2,
-    SYSWAIT     = 3,
-    SYSPIPE     = 4,
-    SYSREAD     = 5,
-    SYSKILL     = 6,
-    SYSEXEC     = 7,
-    SYSFSTAT    = 8,
-    SYSCHDIR    = 9,
-    SYSDUP      = 10,
-    SYSGETPID   = 11,
-    SYSSBRK     = 12,
-    SYSSLEEP    = 13,
-    SYSUPTIME   = 14,
-    SYSOPEN     = 15,
-    SYSWRITE    = 16,
-    SYSMKNOD    = 17,
-    SYSUNLINK   = 18,
-    SYSLINK     = 19,
-    SYSMKDIR    = 20,
-    SYSCLOSE    = 21,
-    SYSSHUTDOWN = 22,
-}
+#![allow(dead_code)]
+
+pub const SYS_FORK       : u64   = 1;
+pub const SYS_EXIT       : u64   = 2;
+pub const SYS_WAIT       : u64   = 3;
+pub const SYS_PIPE       : u64   = 4;
+pub const SYS_READ       : u64   = 5;
+pub const SYS_KILL       : u64   = 6;
+pub const SYS_EXEC       : u64   = 7;
+pub const SYS_FSTAT      : u64   = 8;
+pub const SYS_CHDIR      : u64   = 9;
+pub const SYS_DUP        : u64   = 10;
+pub const SYS_GETPID     : u64   = 11;
+pub const SYS_SBRK       : u64   = 12;
+pub const SYS_SLEEP      : u64   = 13;
+pub const SYS_UPTIME     : u64   = 14;
+pub const SYS_OPEN       : u64   = 15;
+pub const SYS_WRITE      : u64   = 16;
+pub const SYS_MKNOD      : u64   = 17;
+pub const SYS_UNLINK     : u64   = 18;
+pub const SYS_LINK       : u64   = 19;
+pub const SYS_MKDIR      : u64   = 20;
+pub const SYS_CLOSE      : u64   = 21;
+pub const SYS_SHUTDOWN   : u64   = 22;
