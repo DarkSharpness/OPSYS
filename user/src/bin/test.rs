c8795b869c9b8fed1ac915ac9c802403bb0ecf18
DarkSharpness <2040703891@qq.com>
2024-05-15 09:00:21 +0800

    feat(user): complete user program linker for temporary test

---
+++ user/src/bin/test.rs
@@ -0,0 +1,10 @@
+#![no_std]
+#![no_main]
+
+extern crate user_lib;
+
+#[no_mangle]
+fn main() -> i32 {
+
+    return 0;
+}
--- user/src/lib.rs
+++ user/src/lib.rs
@@ -3,3 +3,8 @@
 #![feature(panic_info_message)]
 
 mod syscall;
+
+#[panic_handler]
+fn panic_handler(_: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
